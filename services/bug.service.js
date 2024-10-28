dcda3b09c9833d98becbd1c8a49a4c58efc70b01
eLiranX4411 <solidityart@gmail.com>
2024-10-28 10:51:56 +0200

    create public folder, implement data json, implement bug.service for backend

--- services/bug.service.js
+++ services/bug.service.js
@@ -1,69 +1,54 @@
-
-import { storageService } from './async-storage.service.js'
+import fs from 'fs'
 import { utilService } from './util.service.js'
 
-const STORAGE_KEY = 'bugDB'
+const cars = utilService.readJsonFile('data/car.json')
 
-_createBugs()
-
-export const bugService = {
+export const carService = {
     query,
     getById,
-    save,
     remove,
+    save
 }
 
 
 function query() {
-    return storageService.query(STORAGE_KEY)
-}
-function getById(bugId) {
-    return storageService.get(STORAGE_KEY, bugId)
+    return Promise.resolve(cars)
 }
 
-function remove(bugId) {
-    return storageService.remove(STORAGE_KEY, bugId)
+function getById(carId) {
+    const car = cars.find(car => car._id === carId)
+    if (!car) return Promise.reject('Cannot find car - ' + carId)
+    return Promise.resolve(car)
 }
 
-function save(bug) {
-    if (bug._id) {
-        return storageService.put(STORAGE_KEY, bug)
+function remove(carId) {
+    const carIdx = cars.findIndex(car => car._id === carId)
+    if (carIdx < 0) return Promise.reject('Cannot find car - ' + carId)
+    cars.splice(carIdx, 1)
+    return _saveCarsToFile()
+}
+
+function save(carToSave) {
+
+    if (carToSave._id) {
+        const carIdx = cars.findIndex(car => car._id === carToSave._id)
+        cars[carIdx] = carToSave
     } else {
-        return storageService.post(STORAGE_KEY, bug)
+        carToSave._id = utilService.makeId()
+        cars.unshift(carToSave)
     }
+
+    return _saveCarsToFile().then(() => carToSave)
 }
 
-
-
-
-function _createBugs() {
-    let bugs = utilService.loadFromStorage(STORAGE_KEY)
-    if (!bugs || !bugs.length) {
-        bugs = [
-            {
-                title: "Infinite Loop Detected",
-                severity: 4,
-                _id: "1NF1N1T3"
-            },
-            {
-                title: "Keyboard Not Found",
-                severity: 3,
-                _id: "K3YB0RD"
-            },
-            {
-                title: "404 Coffee Not Found",
-                severity: 2,
-                _id: "C0FF33"
-            },
-            {
-                title: "Unexpected Response",
-                severity: 1,
-                _id: "G0053"
+function _saveCarsToFile() {
+    return new Promise((resolve, reject) => {
+        const data = JSON.stringify(cars, null, 4)
+        fs.writeFile('data/car.json', data, (err) => {
+            if (err) {
+                return reject(err)
             }
-        ]
-        utilService.saveToStorage(STORAGE_KEY, bugs)
-    }
-
-
-
+            resolve()
+        })
+    })
 }

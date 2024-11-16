1a112515e6eabd58baddf163b3a2e87461d8d302
eLiranX4411 <solidityart@gmail.com>
2024-11-16 20:00:41 +0200

    Implement admin control

--- public/cmps/BugList.jsx
+++ public/cmps/BugList.jsx
@@ -6,18 +6,41 @@ import { userService } from '../services/user.service.js'
 export function BugList({ bugs, onRemoveBug, onEditBug, userDetails }) {
   const user = userService.getLoggedinUser()
 
+  function isAllowed(bug) {
+    if (!user) return false
+    if (user.isAdmin) return true
+    if (user._id !== bug.creator._id) return false
+
+    return true
+  }
+
   if (!bugs) return <div>Loading...</div>
+
+  if (!user) {
+    return (
+      <div>
+        <p>
+          You need to log in to manage bugs. Go back <Link to={`/`}>here</Link>.
+        </p>
+      </div>
+    )
+  }
 
   return (
     <ul className='bug-list'>
       {bugs.map((bug) => (
         <li className='bug-preview' key={bug._id}>
           <BugPreview bug={bug} />
-          {user && (
+
+          {isAllowed(bug) ? (
             <div>
               <button onClick={() => onRemoveBug(bug._id)}>x</button>
               <button onClick={() => onEditBug(bug)}>Edit</button>
               <Link to={`/bug/${bug._id}`}>Details</Link>
+            </div>
+          ) : (
+            <div>
+              <p>You are not allowed to edit or delete this bug.</p>
             </div>
           )}
         </li>
